ed6476df4eaa5bf0fb19afd146bc949a63d4266d
Per Johansson <per.johansson@post.com>
2014-02-20 19:48:56 +0000

    renamed to maxTransitionsPerEvent

--- core/src/main/java/org/headstar/scheelite/MaxTransitionsPerEventException.java
+++ core/src/main/java/org/headstar/scheelite/MaxTransitionsPerEventException.java
@@ -3,7 +3,7 @@ package org.headstar.scheelite;
 /**
  * Created by per on 17/02/14.
  */
-public class MaxTransitionsException extends RuntimeException {
+public class MaxTransitionsPerEventException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 }
