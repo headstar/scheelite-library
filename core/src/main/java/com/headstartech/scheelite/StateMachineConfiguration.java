6dc335b2620f315e0c1b5b147ec9cf5107a97b68
Per Johansson <per.johansson@post.com>
2014-11-29 20:49:22 +0000

    added some javadoc

--- core/src/main/java/com/headstartech/scheelite/StateMachineConfiguration.java
+++ core/src/main/java/com/headstartech/scheelite/StateMachineConfiguration.java
@@ -13,17 +13,45 @@ public class StateMachineConfiguration<T, U> {
     private final StateTree<T, U> stateTree;
     private final TransitionMap<T, U> transitionMap;
 
-    public StateMachineConfiguration(StateTree<T, U> stateTree, TransitionMap<T, U> transitionMap) {
+    StateMachineConfiguration(StateTree<T, U> stateTree, TransitionMap<T, U> transitionMap) {
         this.stateTree = stateTree;
         this.transitionMap = transitionMap;
     }
 
+    /**
+     * Gets the states in the state machine
+     *
+     * @return the states
+     *
+     * @see com.headstartech.scheelite.State
+     */
     public Set<State<T, U>> getStates() { return stateTree.getStates(); }
 
+    /**
+     * Gets the transitions in the state machine
+     *
+     * @return the transitions
+     *
+     * @see com.headstartech.scheelite.Transition
+     */
     public Set<Transition<T, U>> getTransitions() { return transitionMap.getTransitions(); }
 
+    /**
+     * Gets the initial transition in the state machine
+     *
+     * @return the initial transitions
+     *
+     * @see com.headstartech.scheelite.Transition
+     */
     public Set<InitialTransition<T, U>> getInitialTransitions() { return null; }
 
+
+    /**
+     * Gets the super state of the specified {@link com.headstartech.scheelite.State}.
+     *
+     * @param state
+     * @return the super state
+     */
     public State<T, U> getSuperState(State<T, U> state) {
         Optional<State<T, U>> superState = stateTree.getParent(state);
         if(superState.isPresent()) {
