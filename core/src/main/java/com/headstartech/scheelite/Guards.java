d6a02c3a986fafcec1f67084f5d1f5a146c89b53
Per Johansson <per.johansson@post.com>
2015-01-29 22:43:52 +0000

    improved Guards toString()

--- core/src/main/java/com/headstartech/scheelite/Guards.java
+++ core/src/main/java/com/headstartech/scheelite/Guards.java
@@ -1,5 +1,6 @@
 package com.headstartech.scheelite;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
@@ -10,36 +11,73 @@ import static com.google.common.base.Preconditions.checkNotNull;
  */
 public class Guards {
 
+    private static final Joiner COMMA_JOINER = Joiner.on(',');
+
     private Guards() {}
 
+    public static <T> Guard<T> and(final Guard<T>... components) {
+        return of(Predicates.<GuardArgs<T>>and(components), new GuardDescription() {
+            @Override
+            public String getDescription() {
+                return "Guards.and(" + COMMA_JOINER.join(components)  + ")";
+            }
+        });
+    }
+
+    public static <T> Guard<T> or(final Guard<T>... components) {
+        return of(Predicates.<GuardArgs<T>>or(components), new GuardDescription() {
+            @Override
+            public String getDescription() {
+                return "Guards.or(" + COMMA_JOINER.join(components)  + ")";
+            }
+        });
+    }
+
+    public static <T> Guard<T> not(final Guard<T> component) {
+        return of(Predicates.<GuardArgs<T>>not(component), new GuardDescription() {
+            @Override
+            public String getDescription() {
+                return "Guards.not(" + component + ")";
+            }
+        });
+    }
+
+
     public static <T> Guard<T> of(Predicate<GuardArgs<T>> pred) {
-        return new GuardPredicate<T>(pred);
+        return new GuardPredicate<T>(pred, null);
     }
 
-    public static <T> Guard<T> and(Guard<T>... components) {
-        return of(Predicates.<GuardArgs<T>>and(components));
+    private static <T> Guard<T> of(Predicate<GuardArgs<T>> pred, GuardDescription guardDescription) {
+        return new GuardPredicate<T>(pred, guardDescription);
     }
 
-    public static <T> Guard<T> or(Guard<T>... components) {
-        return of(Predicates.<GuardArgs<T>>or(components));
-    }
-
-    public static <T> Guard<T> not(Guard<T> component) {
-        return of(Predicates.<GuardArgs<T>>not(component));
+    private static interface GuardDescription {
+        String getDescription();
     }
 
     private static class GuardPredicate<T> implements Guard<T> {
 
         private final Predicate<GuardArgs<T>> pred;
+        private final GuardDescription guardDescription;
 
-        private GuardPredicate(Predicate<GuardArgs<T>> pred) {
+        private GuardPredicate(Predicate<GuardArgs<T>> pred, GuardDescription guardDescription) {
             checkNotNull(pred);
             this.pred = pred;
+            this.guardDescription = guardDescription;
         }
 
         @Override
         public boolean apply(GuardArgs<T> input) {
             return pred.apply(input);
+        }
+
+        @Override
+        public String toString() {
+            if(guardDescription != null) {
+                return guardDescription.getDescription();
+            } else {
+                return pred.toString();
+            }
         }
     }
 }
