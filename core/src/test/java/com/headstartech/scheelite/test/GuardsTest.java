e3fc0721fd613b0015530f265114ecb0c198390d
Per Johansson <per.johansson@post.com>
2015-03-26 22:17:11 +0000

    added guards equals test

--- core/src/test/java/com/headstartech/scheelite/test/GuardsTest.java
+++ core/src/test/java/com/headstartech/scheelite/test/GuardsTest.java
@@ -52,6 +52,33 @@ public class GuardsTest extends TestBase {
 
     @SuppressWarnings("unchecked")
     @Test
+    public void testAndEqualsTrue() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.and(new TestPred(true), new TestPred(false));
+
+        // when
+        boolean res = guard.equals(Guards.and(new TestPred(true), new TestPred(false)));
+
+        // then
+        assertTrue(res);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testAndEqualsFalse() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.and(new TestPred(true), new TestPred(false));
+
+        // when
+        boolean res = guard.equals(Guards.and(new TestPred(true), new TestPred(true)));
+
+        // then
+        assertFalse(res);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Test
     public void testOrTrue() throws Exception {
         // given
         Guard<TestEntity> guard = Guards.or(new TestPred(true), new TestPred(false));
@@ -71,6 +98,32 @@ public class GuardsTest extends TestBase {
 
         // when
         boolean res = guard.evaluate(new TestEntity(), Optional.absent());
+
+        // then
+        assertFalse(res);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testOrEqualsTrue() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.or(new TestPred(true), new TestPred(false));
+
+        // when
+        boolean res = guard.equals(Guards.or(new TestPred(true), new TestPred(false)));
+
+        // then
+        assertTrue(res);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testOrEqualsFalse() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.or(new TestPred(true), new TestPred(false));
+
+        // when
+        boolean res = guard.equals(Guards.or(new TestPred(false), new TestPred(true)));
 
         // then
         assertFalse(res);
@@ -102,6 +155,33 @@ public class GuardsTest extends TestBase {
         assertFalse(res);
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testNotEqualsTrue() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.not(new TestPred(true));
+
+        // when
+        boolean res = guard.equals(Guards.not(new TestPred(true)));
+
+        // then
+        assertTrue(res);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testNotEqualsFalse() throws Exception {
+        // given
+        Guard<TestEntity> guard = Guards.not(new TestPred(true));
+
+        // when
+        boolean res = guard.equals(Guards.not(new TestPred(false)));
+
+        // then
+        assertFalse(res);
+    }
+
+
     private static class TestPred implements Guard<TestEntity> {
 
         private final boolean accept;
@@ -117,6 +197,21 @@ public class GuardsTest extends TestBase {
             return accept;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            TestPred testPred = (TestPred) o;
+
+            return accept == testPred.accept;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return (accept ? 1 : 0);
+        }
     }
 
 }
