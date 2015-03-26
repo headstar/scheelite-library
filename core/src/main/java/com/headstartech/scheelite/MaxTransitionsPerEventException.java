2391e35972399e1a0d9b0e0f41dba4a92a26a2cf
Per Johansson <per.johansson@post.com>
2015-03-26 22:19:01 +0000

    minor javadoc improvement

--- core/src/main/java/com/headstartech/scheelite/MaxTransitionsPerEventException.java
+++ core/src/main/java/com/headstartech/scheelite/MaxTransitionsPerEventException.java
@@ -3,7 +3,7 @@ package com.headstartech.scheelite;
 /**
  * Thrown to indicate that the maximum number of transitions per event was exceeded.
  *
- * @see StateMachine#processEvent(Object, Object, Object)
+ * @see StateMachine#processEvent
  */
 public class MaxTransitionsPerEventException extends RuntimeException {
 
